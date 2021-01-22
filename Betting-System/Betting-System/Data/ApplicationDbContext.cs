74401e426afde499c17e220746d4b2d551d303f2
Martin-Laptop <20162034@atn.ac.at>
2021-01-22 11:48:02 +0100

    Entities

--- Betting-System/Betting-System/Data/ApplicationDbContext.cs
+++ Betting-System/Betting-System/Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Betting_System.Entities;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -11,6 +12,17 @@ namespace Betting_System.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        public DbSet<Bet> Bets { get; set; }
+        public DbSet<Opponent> Opponents { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Bet>()
+                .HasOne<Bet>(b=>b.)
+                .WithMany(s => s.)
         }
     }
 }
---
+++ Betting-System/Betting-System/Entities/OpponentPerBet.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Betting_System.Entities
+{
+    public class OpponentPerBet
+    {
+        [Key]
+        public int? Key { get; set; }
+
+        [Required]
+        public int BetId { get; set; }
+        [Required]
+        public int OponentId { get; set; }
+        [Required]
+        public int Quote { get; set; }
+
+
+        public virtual Opponent Opponent { get; set; }
+        public virtual Bet Bet { get; set; }
+    }
+}
